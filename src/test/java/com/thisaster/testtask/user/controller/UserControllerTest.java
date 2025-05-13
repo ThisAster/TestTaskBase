f7dfa0c4125a9feb91b50d14c40e484d62e07243
ThisAster <freizen034@mail.ru>
2025-05-13 23:34:07 +0300

    edit: user test

--- src/test/java/com/thisaster/testtask/user/controller/UserControllerTest.java
+++ src/test/java/com/thisaster/testtask/user/controller/UserControllerTest.java
@@ -1,14 +1,5 @@
 package com.thisaster.testtask.user.controller;
 
-import com.thisaster.testtask.subscription.dto.SubscriptionDTO;
-import com.thisaster.testtask.subscription.entity.Subscription;
-import com.thisaster.testtask.subscription.mapper.SubscriptionMapper;
-import com.thisaster.testtask.subscription.repository.SubscriptionRepository;
-import com.thisaster.testtask.user.dto.UserDTO;
-import com.thisaster.testtask.user.entity.User;
-import com.thisaster.testtask.user.mapper.UserMapper;
-import com.thisaster.testtask.user.repository.UserRepository;
-import com.thisaster.testtask.user.service.UserService;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -23,9 +14,6 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.StringContains.containsString;
@@ -45,43 +33,19 @@ public class UserControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    SubscriptionRepository subscriptionRepository;
-
-    @Autowired
-    UserMapper userMapper;
-
-    @Autowired
-    SubscriptionMapper subscriptionMapper;
-
     @Value("${auth-service.token}")
     String token;
 
     @Test
     @Transactional
     void shouldGetUserById() throws Exception {
-        User user = userRepository.findById(1L).get();
-        UserDTO userDTO = userMapper.toDTO(user);
-        Subscription subscription = subscriptionRepository.findById(1L).get();
-        SubscriptionDTO subscriptionDTO = subscriptionMapper.toDTO(subscription);
-        userService.subscribeUserToSub(1L, subscription);
-        Set<SubscriptionDTO> subscriptionDTOs = new HashSet<>();
-        subscriptionDTOs.add(subscriptionDTO);
-        userDTO.setSubscriptions(subscriptionDTOs);
+        Long userId = 1L;
 
-        MvcResult mvcResult = mockMvc.perform(get("/api/users/1")
+        MvcResult mvcResult = mockMvc.perform(get("/api/users/{id}", userId)
                         .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(1)))
                 .andExpect(jsonPath("$.username", is("admin")))
-                .andExpect(jsonPath("$.subscriptions[0].id", is(1)))
-                .andExpect(jsonPath("$.subscriptions[0].name", is("Premium Plan")))
                 .andReturn();
 
         log.info(mvcResult.getResponse().getContentAsString());
@@ -101,7 +65,6 @@ public class UserControllerTest {
     @Test
     void shouldDeleteUserById() throws Exception {
         Long userId = 3L;
-        log.info(userRepository.findById(userId).get().getUsername());
         mockMvc.perform(delete("/api/users/{id}", userId)
                         .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
